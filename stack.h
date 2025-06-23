bc794c3b00d404a0fc226e0f325ae355055bacd6
WesllyBraga <161134362+WesllyBraga@users.noreply.github.com>
2025-06-23 15:07:16 -0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,12 +1,11 @@
-#ifndef stack_h
-#define stack_h
+#ifndef STACK_H
+#define STACK_H
 
 typedef struct stack Stack;
 
 Stack* new_stack(int size);
 void stack_push(Stack* s, int value);
-int stack_pop (Stack* s);
+int stack_pop(Stack* s);
 void stack_print(Stack* s);
 
-
-#endif 
+#endif
