4cdfdb70645da5543b4938feffea0530ea586ce1
WesllyBraga <161134362+WesllyBraga@users.noreply.github.com>
2025-06-23 15:06:53 -0300

    Update stack.c

--- stack.c
+++ stack.c
@@ -2,67 +2,52 @@
 #include <stdlib.h>
 #include "stack.h"
 
-struct stack
-{
+struct stack {
     int *values;
     int capacity;
     int top;
 };
 
-Stack *new_stack(int size)
-{
-    Stack *stack = (Stack *)malloc(sizeof(Stack));
-    if (!stack)
-        return NULL;
+Stack* new_stack(int size) {
+    Stack* stack = (Stack*) malloc(sizeof(Stack));
+    if (!stack) return NULL;
 
-    stack->values = (int *)malloc(size * sizeof(int));
-
-    if (!stack->values)
-    {
+    stack->values = (int*) malloc(size * sizeof(int));
+    if (!stack->values) {
         free(stack);
         return NULL;
     }
 
-    stack->top = -1;
     stack->capacity = size;
+    stack->top = -1;
 
     return stack;
 }
 
-void stack_push(Stack *s, int value)
-{
-    if (s->top >= s->capacity - 1)
-    {
+void stack_push(Stack* s, int value) {
+    if (s->top >= s->capacity - 1) {
         printf("Pilha cheia\n");
         return;
     }
-    else
-    {
-        s->values[++s->top] = value;
-        printf("Valor %d empilhado.\n", value);
-    }
+    s->values[++s->top] = value;
+    printf("Valor %d empilhado.\n", value);
 }
 
-int stack_pop(Stack *s)
-{
-    if (s->top < 0)
-    {
+int stack_pop(Stack* s) {
+    if (s->top < 0) {
         printf("Erro: pilha vazia!\n");
-        return -1;
+        return -1; // sinal de erro
     }
-    return s->values[(s->top)--];
+    return s->values[s->top--];
 }
 
-void stack_print(Stack *s)
-{
-    if (s->top < 0)
-    {
+void stack_print(Stack* s) {
+    if (s->top < 0) {
         printf("Pilha vazia.\n");
         return;
     }
     printf("Pilha: ");
-    for (int i = 0; i <= s->top; i++)
-    {
+    for (int i = 0; i <= s->top; i++) {
         printf("%d ", s->values[i]);
     }
     printf("\n");
