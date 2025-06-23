5678ce05843598aa865bb2128c26f63099ee2630
WesllyBraga <161134362+WesllyBraga@users.noreply.github.com>
2025-06-23 15:02:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "interpret.h"
 
-static void repl()
-{
-  char line[1024];
-  for (;;)
-  {
-    printf("> ");
+#define MAX_LINE_LENGTH 256
 
-    if (!fgets(line, sizeof(line), stdin))
-    {
-      printf("\n");
-      break;
+static void repl() {
+    char line[MAX_LINE_LENGTH];
+    for (;;) {
+        printf("> ");
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        interpret(line);
     }
-
-    interpret(line);
-  }
 }
 
+int interpret_file(const char* path) {
+    FILE* file = fopen(path, "r");
+    if (!file) {
+        perror(path);
+        return EXIT_FAILURE;
+    }
 
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file)) {
+        interpret(line);
+    }
 
+    if (fclose(file)) {
+        perror(path);
+        return EXIT_FAILURE;
+    }
 
-int main () {
+    return 0;
+}
+
+int main(int argc, char** argv) {
     interpreter_init();
-    repl();
+
+    if (argc == 1) {
+        repl();
+    } else {
+        interpret_file(argv[1]);
+    }
     return 0;
 }
